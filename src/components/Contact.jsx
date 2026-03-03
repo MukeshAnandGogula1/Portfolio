9793c9ca456149a2972e9d0466307e7cffddeb83
G.Mukesh Anand <fallingmoon010205@gmail.com>
2026-03-03 16:23:11 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,24 +9,24 @@ const Contact = () => {
         <p>If you'd like to get in touch, feel free to reach out via any of the options below or
         send a quick message using the form.</p>
 
-        <div className="links">
-        <a
-          href="https://www.linkedin.com/in/mukesh-anand-gogula-495115291"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          🔗 LinkedIn
-        </a>
-        <a
-          href="https://github.com/MukeshAnandGogula1"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          🐙 GitHub
-        </a>
-        <a href="mailto:mukeshanandgogula@gmail.com">✉️ Email</a>
-        <a href="#contact">📩 Contact</a>
-      </div>
+    <div className="links">
+     <a
+      href="https://www.linkedin.com/in/mukesh-anand-gogula-495115291"
+      target="_blank"    
+      rel="noopener noreferrer"
+    >         
+    🔗 LinkedIn
+     </a>
+     <a
+      href="https://github.com/MukeshAnandGogula1"
+      target="_blank"
+      rel="noopener noreferrer"
+     >
+    🐙 GitHub
+     </a>
+     <a href="mailto:mukeshanandgogula@gmail.com">✉️ Email</a>
+     <a href="#contact">📩 Contact</a>
+    </div>
         <form
           className="contact-form"
           action="mailto:mukeshanandgogula@gmail.com"

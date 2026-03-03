08ee0d9b989bcdaf45f4f2779dba6b2917111a9e
G.Mukesh Anand <fallingmoon010205@gmail.com>
2026-03-03 15:18:46 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -16,7 +16,7 @@ const Profile = () => {
           <h2>Mukesh Anand Gogula</h2>
           <p>
             B.Tech AI & ML student passionate about building intelligent systems,
-            solving real-world problems, and exploring cybersecurity, deep learning,
+            solving real-world problems, and exploring new Tools, Deep learning,
             and full-stack development.
           </p>
         </div>
